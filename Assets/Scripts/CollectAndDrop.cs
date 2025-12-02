b8cd8e704dc08f930cb58210553c7bf57a44ddee
ela-artukovic-tus23 <K00299459@student.tus.ie>
2025-12-02 18:15:54 +0000

    start of adding lmb and rmb

---
+++ Assets/Scripts/CollectAndDrop.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CollectAndDrop : MonoBehaviour
+{
+    public InventoryManager invManager;
+    private CollectableItem itemTC;             //named it TC for TO COLLECT
+    private bool inRange = false;            //need for player check 
+
+    public void OnCollect(InputAction.CallbackContext context)          //set LMB for collecting and RMB for dropping
+    {
+        if (context.performed && inRange)             //if LMB is presed and the player is in range then collect item
+        {
+            CollectItem();
+        }
+    }
+
+    public void OnDrop(InputAction.CallbackContext context)
+    {
+        if (context.performed)               //if RMB is pressed then drop item
+        {
+            DropItem();
+        }
+    }
+
+    private void CollectItem()
+    {
+        if (itemTC != null)          
+        {
+
+        }
+    }
+
+    private void DropItem()
+    {
+
+    }
+   
+
+
+}
--- Assets/Scripts/CollectableInteractor.cs
+++ Assets/Scripts/CollectableInteractor.cs
@@ -22,23 +22,7 @@ public class CollectableInteractor : MonoBehaviour           //handle interactio
         }
     }
 
-    //public void RemoveItem(InventoryItem theItem)      
-    //{
-    // if (theItem != null)                //check if item isn't null
-    //{
-    // if (theItem.quantity > 1)             //if the current quantity of the item is bigger than one
-    //{
-    // theItem.quantity -= 1;            //take away one
-    // }
-    //else
-    //{
-    // theItem.quantity = 0;             //if its not bigger than 1 so equal to 1 and then removed, set quantity to 0
-    //InventoryItem.Remove(theItem);    //remove the item from inventory
-    // }
-    // }                                                 //need to instantiate spawned item from prefab - get component from collectable controller - itemSO 
-    //after instantiating need to get the component for setting color from collectable controller
-    // }                                                     //need to invoke event 
-
+   
 
     private void OnTriggerEnter(Collider other)        //if player collides with gameobjects
     {
