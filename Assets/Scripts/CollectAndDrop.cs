81ec30af19b6feee8d10dc6e216e07b7e4d6afa0
ela-artukovic-tus23 <K00299459@student.tus.ie>
2025-12-02 18:36:27 +0000

    WGY DONT U WORK DAMN U DGSDVB

--- Assets/Scripts/CollectAndDrop.cs
+++ Assets/Scripts/CollectAndDrop.cs
@@ -25,10 +25,10 @@ public class CollectAndDrop : MonoBehaviour
 
     private void CollectItem()
     {
-        if (itemTC != null)                   //if item is collected and its not null, add the item to collect to the inventory 
+        if (itemTC != null)                   //if an iten can be collected/not null, add the item to collect to the inventory using InvMan logic
         {
             invManager.ItemAdded(itemTC);
-            itemTC = null;
+            itemTC = null;                       //reset it so more can be collected and it doesn't think the item i collected is still there
             inRange = false;
         }
     }
@@ -40,7 +40,7 @@ public class CollectAndDrop : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        CollectableItem collectable = other.GetComponent<CollectableItem>();
+        CollectableItem collectable = other.GetComponent<CollectableItem>();           //get collectible item component from other object, if it has one then set TC to the collectible and make it in range
         if (collectable != null)
         {
             itemTC = collectable;
@@ -50,7 +50,7 @@ public class CollectAndDrop : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        CollectableItem collectable = other.GetComponent<CollectableItem>();
+        CollectableItem collectable = other.GetComponent<CollectableItem>();            //if item leaving is same as one in range(TC == collectable) then clear it and set it no longer in range 
         if (collectable == itemTC)
         {
             itemTC = null;
